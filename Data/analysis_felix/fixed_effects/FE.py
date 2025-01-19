61039e6375dae4c0f58650ca2183faebcf5a7073
Felix Schlechte <felix.schlechte@t-online.de>
2025-01-19 14:46:36 +0100

    fixed effects

---
+++ Data/analysis_felix/fixed_effects/FE.py
@@ -0,0 +1,42 @@
+import pandas as pd
+from linearmodels.panel import PanelOLS
+import matplotlib.pyplot as plt
+
+file = r'C:\Users\Home\Documents\M.Sc.ML\Data Literacy\analysis_felix\elec_NO2_filtered.csv'
+traffic_file = r'C:\Users\Home\Documents\M.Sc.ML\Data Literacy\analysis_felix\traffic.csv'
+
+df = pd.read_csv(file)
+
+df_traffic = df[df['Air Quality Station Type'] == 'Traffic']
+df_background = df[df['Air Quality Station Type'] == 'Background']
+df_industrial = df[df['Air Quality Station Type'] == 'Industrial']
+
+# print((len(df_traffic) + len(df_background) + len(df_industrial)) == len(df)) # this is True
+
+df_traffic.to_csv(traffic_file, index=False, encoding="utf-8")
+
+df_traffic = pd.read_csv(traffic_file)
+
+df_traffic = df_traffic.set_index(['City', 'Year'])
+
+# Fixed Effects Modell
+mod = PanelOLS.from_formula(
+    'Air_Pollution_Level ~ Gruene + CDU + EntityEffects + TimeEffects',
+    df_traffic
+)
+
+# Modell schätzen
+res = mod.fit()
+
+# Ergebnisse anzeigen
+print(res.summary)
+
+
+
+# Koeffizienten extrahieren
+coefficients = res.params[:-1]  # EntityEffects wird ausgeschlossen
+coefficients.plot(kind='bar')
+plt.title("Einfluss der Parteien auf die Luftqualität")
+plt.ylabel("Koeffizient")
+plt.show()
+
