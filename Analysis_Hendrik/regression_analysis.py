7dfc409e10e298890ecb5295db5f3c5b68ea8c80
Hendrik Jansen <hendrikjansen@posteo.net>
2025-01-23 16:16:28 +0100

    do the analysis over all parties and pollutants

--- Analysis_Hendrik/regression_analysis.py
+++ Analysis_Hendrik/regression_analysis.py
@@ -1,43 +1,66 @@
 import pandas as pd
 from scipy import stats
+import statsmodels.api as sm
 import numpy as np
 from matplotlib import pyplot as plt
+import seaborn as sns
+
+
+def process_data(party, pollutant, offset):
+    election_data = pd.read_csv('../Data/election_data.csv')
+    pollutant_data = pd.read_csv(f'../Data/AirPollution/processed/{pollutant}.csv')
+    pollutant_data = pollutant_data[['Air Quality Station EoI Code', 'Year', 'Air Pollution Level', 'City']]
+    pollutant_data = pollutant_data.groupby(['Air Quality Station EoI Code', 'Year', 'City'], as_index=False).mean()
+
+    data = pd.merge(election_data, pollutant_data, left_on=['City', 'Date'], right_on=['City', 'Year'])
+
+    data = data[['Air Quality Station EoI Code', party, 'Date', 'Air Pollution Level', 'City']]
+
+    data = data[data[party] != 0]
+    data['Date'] = pd.to_numeric(data['Date'])
+
+    x = []
+    y = []
+
+    for index, row in data.iterrows():
+        station = row['Air Quality Station EoI Code']
+        year = row['Date']
+        if pollutant_data[(pollutant_data['Air Quality Station EoI Code'] == station) & (pollutant_data['Year'] == year + offset)].any()['Air Pollution Level'] and year + offset < 2025:
+            election_result = row[party]
+            pollution_before = row['Air Pollution Level']
+            pollution_after = pollutant_data[(pollutant_data['Air Quality Station EoI Code'] == station) & (pollutant_data['Year'] == year + offset)]['Air Pollution Level'].values[0]
+            pollution_change = pollution_after - pollution_before
+            x.append(election_result)
+            y.append(pollution_change)
+    return np.array(x), np.array(y)
+    
+def analysis_and_plot(x, y, axs):
+    N = len(x)
+
+    statistic = lambda x, y: stats.linregress(x, y).rvalue
+    res = stats.permutation_test(data=(x, y), statistic=statistic, permutation_type='pairings')
+
+    res = stats.linregress(x, y)
+    axs.plot(x, y, 'o', label='original data')
+    axs.plot(x, res.intercept + res.slope*x, 'r', label='fitted line')
 
 offset = 5
-party = 'SPD'
+parties = ['Linke', 'Gruene', 'SPD', 'FDP', 'CDU', 'AfD']
+pollutants = ['CO', 'NO2', 'O3', 'PM10', 'SO2']
 
-election_data = pd.read_csv('../Data/election_data.csv')
-PM10_data = pd.read_csv('../Data/AirPollution/processed/PM10.csv')
-PM10_data = PM10_data[['Air Quality Station EoI Code', 'Year', 'Air Pollution Level', 'City']]
-PM10_data = PM10_data.groupby(['Air Quality Station EoI Code', 'Year', 'City'], as_index=False).mean()
+fig, axs = plt.subplots(len(pollutants), len(parties))
 
-data = pd.merge(election_data, PM10_data, left_on=['City', 'Date'], right_on=['City', 'Year'])
+for i, pollutant in enumerate(pollutants):
+    for j, party in enumerate(parties):
+        x, y = process_data(party, pollutant, offset)
+        analysis_and_plot(x, y, axs[i, j])
 
-data = data[['Air Quality Station EoI Code', party, 'Date', 'Air Pollution Level', 'City']]
+        print(f'Pollutant {pollutant} for {party} done!')
 
-data = data[data[party] != 0]
-data['Date'] = pd.to_numeric(data['Date'])
+for i, party in enumerate(parties):
+    axs[len(pollutants)-1][i].set_xlabel(f'{party}')
 
-x = []
-y = []
+for i, pollutant in enumerate(pollutants):
+    axs[i][0].set_ylabel(f'{pollutant}')
 
-for index, row in data.iterrows():
-    station = row['Air Quality Station EoI Code']
-    year = row['Date']
-    if PM10_data[(PM10_data['Air Quality Station EoI Code'] == station) & (PM10_data['Year'] == year + offset)].any()['Air Pollution Level'] and year + offset < 2025:
-        election_result = row[party]
-        pollution_before = row['Air Pollution Level']
-        pollution_after = PM10_data[(PM10_data['Air Quality Station EoI Code'] == station) & (PM10_data['Year'] == year + offset)]['Air Pollution Level'].values[0]
-        pollution_change = pollution_after - pollution_before
-        x.append(election_result)
-        y.append(pollution_change)
-
-print(len(x))
-
-x = np.array(x)
-y = np.array(y)
-res = stats.linregress(x, y)
-plt.plot(x, y, 'o', label='original data')
-plt.plot(x, res.intercept + res.slope*x, 'r', label='fitted line')
-plt.legend()
 plt.show()
